9c08a63d764de54fb106ca0d2d0600549d4b844f
Girishmasade <girishmasade@gmail.com>
2025-07-17 22:43:42 +0530

    feat: update index.html title and favicon; modify QuickActions component layout and remove unused icon
    Signed-off-by: Girishmasade <girishmasade@gmail.com>

--- client/src/components/owner/QuickActions.jsx
+++ client/src/components/owner/QuickActions.jsx
@@ -1,15 +1,15 @@
-import { Calendar, ChartColumn, Settings, UserPlus } from 'lucide-react';
+import { ChartColumn, Settings, UserPlus } from 'lucide-react';
 import React from 'react';
 
 const QuickActions = () => {
   return (
-    <div className="flex flex-col justify-between bg-transparent border border-gray-800 mt-4 p-4 rounded-md">
+    <div className="flex flex-col justify-center bg-transparent border border-gray-800 mt-4 p-4 rounded-md">
       <div className="flex flex-col gap-1">
         <h1 className="text-2xl text-white">Quick Actions</h1>
         <span className="text-gray-400 text-md">Common administrative tasks</span>
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 pt-5">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-3 pt-5">
         <button className="flex flex-col items-center justify-center gap-2 border rounded-md hover:bg-green-600 cursor-pointer hover:text-black text-green-400 border-green-700 h-[90px] text-sm">
           <UserPlus className="w-5 h-5" />
           Manage Plans
@@ -18,11 +18,6 @@ const QuickActions = () => {
         <button className="flex flex-col items-center justify-center gap-2 border rounded-md hover:bg-cyan-600 cursor-pointer hover:text-black text-cyan-400 border-cyan-700 h-[90px] text-sm">
           <UserPlus className="w-5 h-5" />
           Add Users
-        </button>
-
-        <button className="flex flex-col items-center justify-center gap-2 border rounded-md hover:bg-cyan-600 cursor-pointer hover:text-black text-cyan-400 border-cyan-700 h-[90px] text-sm">
-          <Calendar className="w-5 h-5" />
-          Schedule Class
         </button>
 
         <button className="flex flex-col items-center justify-center gap-2 border rounded-md hover:bg-cyan-600 cursor-pointer hover:text-black text-cyan-400 border-cyan-700 h-[90px] text-sm">
